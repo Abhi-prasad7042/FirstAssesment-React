c59530786dfaec93042e1b8dc8d805f5803886c6
Abhi-prasad7042 <abhiprasad7042@gmail.com>
2024-03-09 13:40:34 +0530

    first assesment

---
+++ App.js
@@ -0,0 +1,3 @@
+let head = React.createElement("h1", {"id":"head"}, "hello from react of App")
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(head)
---
+++ readMe.md
@@ -0,0 +1,6 @@
+# Steps:
+
+- First creat heading using simple HTML 
+- Second create a heading using JavaScript
+- Third create a heading using REACT and REACT CDN links
+
